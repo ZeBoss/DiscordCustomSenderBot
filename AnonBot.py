8d6f4a8ead82aa32bc8749cebf427137c9df5d9f
ZeBoss <bstamp72@gmail.com>
2026-03-11 13:20:54 +0000

    Registration works.

--- AnonBot.py
+++ AnonBot.py
@@ -31,7 +31,7 @@ class aclient(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced: #check if slash commands have been synced 
-            await tree.sync(guild = discord.Object(id="")) #guild specific: leave blank if global (global registration can take 1-24 hours)
+            await tree.sync() #(guild=discord.Object(id="1043885670629900288")) #guild specific: leave blank if global (global registration can take 1-24 hours)
             self.synced = True
         print(f"We have logged in as {self.user}.")
 
@@ -44,17 +44,15 @@ async def on_message(message):
         if (message.author.bot):
             return
 
-tree.command(name = 'register', description='Register your character') #guild specific slash command
-#@app_commands.checks.cooldown(1, 180.0, key=lambda i: (i.guild_id, i.user.id))
+@tree.command(name = 'register', description='Register your character') #guild specific slash command
 @app_commands.describe(char_name = "Your character name", avatar_url = "Your character avatar url")
 async def register(interaction: discord.Interaction, char_name: str, avatar_url: str):
     try:
         Id = str(interaction.user.id)
-        sql = "INSERT INTO userdata (user_id, char_name, avatar_url) VALUES (%s, %s, %s)"
+        sql = "REPLACE INTO userdata (user_id, char_name, char_pic) VALUES (%s, %s, %s)"
         val = (Id, char_name, avatar_url)
         cursor.execute(sql, val)
         mydb.commit()
-        
         await interaction.response.send_message(f"Registration successful!", ephemeral = True)
     except:
         await interaction.response.send_message(f"Registration failed.", ephemeral = True)
@@ -63,7 +61,7 @@ async def register(interaction: discord.Interaction, char_name: str, avatar_url:
 @tree.command(name = 'speak', description='Speak!') #guild specific slash command
 #@app_commands.checks.cooldown(1, 180.0, key=lambda i: (i.guild_id, i.user.id))
 @app_commands.describe(whattosay = "What do you want to say?")
-async def speak(interaction: discord.Interaction, whattosay: str):
+async def slash2(interaction: discord.Interaction, whattosay: str):
     #getting user details
 
     await interaction.response.send_message(content="Sending...", ephemeral = True, delete_after = 0)
@@ -101,13 +99,13 @@ async def on_test_error(interaction: discord.Interaction, error: app_commands.Ap
     if isinstance(error, app_commands.CommandOnCooldown):
         await interaction.response.send_message(str(error), ephemeral=True)
         
-@tree.command( name = 'help', description='Learn"') #guild specific slash command
+@tree.command( name = 'help', description='Learn') #guild specific slash command
 async def help(interaction: discord.Interaction):
     await interaction.response.send_message(f"DM me anything for how to register!", ephemeral = True)
 
-@client.event
-async def on_ready():
-    await tree.sync(guild=discord.Object(id="1043885670629900288")) #guild specific: leave blank if global (global registration can take 1-24 hours)
+#@client.event
+#async def on_ready():
+#    await tree.sync(guild=discord.Object(id="1043885670629900288")) #guild specific: leave blank if global (global registration can take 1-24 hours)
 
 
     #sql = "SELECT char_name FROM userdata WHERE user_id=(%s)"
